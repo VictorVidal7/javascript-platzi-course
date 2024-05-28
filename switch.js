3d5096ea78e724c0689aed097a7bbce7e1827733
Victor Vidal <victor_vdu@hotmail.com>
2024-05-27 23:48:32 -0600

    Adding a new exercise

---
+++ switch.js
@@ -0,0 +1,32 @@
+/* En este desafío debes retornar un mensaje distinto dependiendo del artículo de tecnología que recibas:
+
+Si recibes una "computadora", debes retornar el mensaje "Con mi computadora puedo programar usando JavaScript".
+Si recibes un "celular", debes retornar el mensaje "En mi celular puedo aprender usando la app de Platzi".
+Si recibes un "cable", debes retornar el mensaje "¡Hay un cable en mi bota!".
+Y si no recibes ninguno de estos valores, debes retornar el mensaje "Artículo no encontrado".
+Input
+
+solution('computadora')
+solution('celular')
+solution('cable')
+solution('ornitorrinco')
+
+Output
+
+Con mi computadora puedo programar usando JavaScript
+En mi celular puedo aprender usando la app de Platzi
+¡Hay un cable en mi bota!
+Artículo no encontrado */
+
+function solution(article) {
+    switch (article) {
+        case 'computadora':
+        return 'Con mi computadora puedo programar usando JavaScript';
+        case 'celular':
+        return 'En mi celular puedo aprender usando la app de Platzi';
+        case 'cable':
+        return '¡Hay un cable en mi bota!';
+        default:
+        return 'Artículo no encontrado';
+    }
+    }
